67dc683043a814eeed82322804d64b0a50beb7f8
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2022-11-09 01:28:58 +0600

    JS eCommerce
    JS eCommerce

---
+++ public/app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+import ProductScreen from "./screen/ProductScreen.js";
+import HomeScreen from "./screen/HomeScreens.js";
+import Error404Screen from "./screen/Error404Screen.js";
+import { parseRequestUrl } from "./utils.js";
+
+const routes = {
+    "/": HomeScreen,
+    "/product/:id": ProductScreen,
+};
+
+const router = () => {
+    const request = parseRequestUrl();
+    const parseUrl =
+        (request.resource ? `/${request.resource}` : "/") +
+        (request.id ? "/:id" : "") +
+        (request.verb ? `/${request.verb}` : "");
+
+    const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+
+    const main = document.getElementById("main-container");
+    main.innerHTML = screen.render();
+};
+
+window.addEventListener("load", router);
+window.addEventListener("hashchange", router);
